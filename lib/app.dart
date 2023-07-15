030440066c2c6933aab354760b05548af1784a83
Milan Chabhadiya <mchabhadiya5@gmail.com>
2023-07-15 21:13:10 +0530

    router-setup completed

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/router.dart';
 import 'package:news_app/utils/theme.dart';
 
 class NewsApp extends StatelessWidget {
@@ -6,10 +7,10 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const Scaffold(),
+      routerConfig: AppRouter.instance.router,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  factory AppRouter() => instance;
+  AppRouter._internal();
+  static final AppRouter instance = AppRouter._internal();
+  // static final _rootNavigatorKey = GlobalKey<NavigatorState>();
+  // static final _shellNavigatorKey = GlobalKey<NavigatorState>();
+  final router = GoRouter(
+    routes: [
+      ShellRoute(
+        builder: (_, state, child) => Scaffold(body: child),
+        routes: [
+          AppRoute(
+            path: '/',
+            routes: [
+              AppRoute(
+                path: 'home',
+                builder: (s) => const Center(
+                  child: Text('HomePage'),
+                ),
+                useFade: true,
+              ),
+              AppRoute(
+                path: 'home1',
+                builder: (s) => const Center(
+                  child: Text('HomePage'),
+                ),
+              ),
+            ],
+            builder: (s) => Builder(
+              builder: (context) {
+                return Center(
+                  child: Column(
+                    children: [
+                      const Text('Milan'),
+                      ElevatedButton(
+                        onPressed: () => context.go('/home'),
+                        child: const Text('HomePage'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () => context.go('/home1'),
+                        child: const Text('HomePage1'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            useFade: true,
+          ),
+        ],
+      )
+    ],
+  );
+}
+
+/// Custom GoRoute sub-class to make the router declaration easier to read
+class AppRoute extends GoRoute {
+  AppRoute({
+    required super.path,
+    required Widget Function(GoRouterState s) builder,
+    bool useFade = false,
+    List<GoRoute> routes = const [],
+  }) : super(
+          routes: routes,
+          pageBuilder: (context, state) {
+            final child = Scaffold(
+              body: builder(state),
+            );
+            if (useFade) {
+              return CustomTransitionPage(
+                child: child,
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+              );
+            }
+            return MaterialPage(child: child);
+          },
+        );
+}
